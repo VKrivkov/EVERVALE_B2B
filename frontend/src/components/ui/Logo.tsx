e39fc53986dcc7f57111185db7b50f3e2a535b2b
Yaroslav <166743450+yaroslavmuzalevskyi@users.noreply.github.com>
2026-03-09 11:53:09 +0100

    deploy fix

--- frontend/src/components/ui/Logo.tsx
+++ frontend/src/components/ui/Logo.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import Image from "next/image";
-import EvervaleLogo from "@/public/logo.svg";
 
 const Logo = () => {
-  return (
-    <Image src={EvervaleLogo} alt="Evervale Logo" width={130} height={40} />
-  );
+  return <Image src="/logo.svg" alt="Evervale Logo" width={130} height={40} />;
 };
 
 export default Logo;
